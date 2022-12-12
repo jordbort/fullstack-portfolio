02af48bb30fb367a3051e23079427b4008b79608
jordbort <jordan.bortner@gmail.com>
2022-12-12 15:00:48 -0500

    Building navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom"
+
 function Header(props) {
-    return <h1>Header</h1>
+    // Styling
+    const navStyle = {
+        display: "flex",
+        justifyContent: "space-around",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: "auto"
+    }
+
+    return (
+        <header>
+            <h1>My portfolio page</h1>
+            <nav style={navStyle}>
+                <div className="nav-link">
+                    <Link to='/'>HOME</Link>
+                </div>
+                <div className="nav-link">
+                    <Link to='/about'>ABOUT</Link>
+                </div>
+                <div className="nav-link">
+                    <Link to='/projects'>PROJECTS</Link>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
