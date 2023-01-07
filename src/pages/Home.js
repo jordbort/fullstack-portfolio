735c1c07eff85f5d949f938526f43fc1949d4364
jordbort <jordan.bortner@gmail.com>
2023-01-07 01:39:48 -0500

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ function Home(props) {
         <main>
             <p>▓▒░▒▓▒░▒▓▒░▒▓</p>
             <h1>Welcome to Jordan Bortner's fullstack development portfolio</h1>
-            <p>▓▒░▒▓▒░▒▓▒░▒▓</p>
+            <p>░▒▓▒░▒▓▒░▒▓▒░</p>
             <p>Please be sure to check out the About and Projects pages for more information!</p>
         </main>
     )
