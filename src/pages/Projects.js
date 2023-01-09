69b6b7d000d6d94d5fda5ba3803b7ae68efe9bb7
jordbort <jordan.bortner@gmail.com>
2023-01-08 21:43:48 -0500

    Making repo lower-case

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -22,8 +22,8 @@ function Projects() {
                         {project.description ? <p>{project.description}</p> : null}
                         {project.image ? <img src={project.image} alt={`Screenshot from ${project.name}`} /> : null}
                         <div className="buttons-container">
-                            {project.gitFrontEnd ? <a href={project.gitFrontEnd} target="_blank" rel="noreferrer"><button>Frontend Repo<span className="desktop-only"> on GitHub</span></button></a> : null}
-                            {project.gitBackEnd ? <a href={project.gitBackEnd} target="_blank" rel="noreferrer"><button>Backend Repo<span className="desktop-only"> on GitHub</span></button></a> : null}
+                            {project.gitFrontEnd ? <a href={project.gitFrontEnd} target="_blank" rel="noreferrer"><button>Frontend repo<span className="desktop-only"> on GitHub</span></button></a> : null}
+                            {project.gitBackEnd ? <a href={project.gitBackEnd} target="_blank" rel="noreferrer"><button>Backend repo<span className="desktop-only"> on GitHub</span></button></a> : null}
                             {project.live ? <a href={project.live} target="_blank" rel="noreferrer"><button>Deployed site</button></a> : null}
                         </div>
                     </section>
