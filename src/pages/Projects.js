2f6c023862fdfa93d4899e6a6220918a6799b377
jordbort <jordan.bortner@gmail.com>
2023-01-14 00:14:33 -0500

    Updated with desktop and mobile images when available

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,36 +1,33 @@
 import { useEffect, useState } from "react"
 
 function Projects(props) {
-    // Create state to hold "Projects" data
     const [projects, setProjects] = useState(null)
 
-    // Create function to make API call
     const getProjectsData = async () => {
-
-        // Make API call and wait for the response
         const response = await fetch("./projects.json")
-
-        // Turn response into JavaScript object
         const data = await response.json()
-
-        // Set the About state to the data
         setProjects(data)
     }
 
-    // Make an initial call for the data inside a useEffect, so it only happens once, on component load
     useEffect(() => {
         getProjectsData()
     }, [])
 
-    // Define a function that will return the JSX needed once we get the data
     const loaded = () => {
         return (
             <main>
                 {projects.map((project, idx) => (
                     <section key={idx}>
                         {project.name ? <h4>{project.name}</h4> : null}
-                        {project.image ? <img src={project.image} alt={`Screenshot from ${project.name}`} /> : null}
-                        <div className="buttons-container">
+                        {project.desktop || project.mobile ? <div className="preview-images-container">
+                            {project.desktop ? <div className="desktop-preview">
+                                <img src={project.desktop} alt={`Screenshot from ${project.name}`} />
+                            </div> : null}
+                            {project.mobile ? <div className="mobile-preview">
+                                <img src={project.mobile} alt={`Screenshot from ${project.name}`} />
+                            </div> : null}
+                        </div> : null}
+                        {project.gitFrontEnd || project.gitBackEnd || project.live ? <div className="buttons-container">
                             {project.gitFrontEnd ? <a href={project.gitFrontEnd} target="_blank" rel="noreferrer">
                                 <button>GitHub Frontend Repo</button>
                             </a> : null}
@@ -40,14 +37,13 @@ function Projects(props) {
                             {project.live ? <a href={project.live} target="_blank" rel="noreferrer">
                                 <button>Deployed site</button>
                             </a> : null}
-                        </div>
+                        </div> : null}
                     </section>
                 ))}
             </main>
         )
     }
 
-    // If data arrives, return the result, otherwise a loading screen
     return projects ? loaded() : <h1>Loading...</h1>
 }
 
