57a8c4e94aeec5443075b8ce9c372add396d764b
jordbort <jordan.bortner@gmail.com>
2023-01-22 21:50:41 -0500

    Adding photo, removing redundant info, and adding email link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,12 +17,11 @@ export default function About() {
     function loaded() {
         return (
             <main>
+                <img src="jordan-bortner.png" className="about-photo" alt="Jordan Bortner" />
                 <p>&#10216;&#10216;</p>
                 <h3>Name: {about.name}</h3>
                 <p>Location: {about.location}</p>
-                <p>GitHub: <a href={about.github} target="_blank" rel="noopener noreferrer">github.com/jordbort</a></p>
-                <p>LinkedIn: <a href={about.linkedin} target="_blank" rel="noopener noreferrer">in/jordan-bortner/</a></p>
-                <p>Bio: {about.bio}</p>
+                <p className="bio">Bio: {about.bio}</p>
                 <p>&#10217;&#10217;</p>
             </main>
         )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
             <p>▓▒░▒▓▒░▒▓▒░▒▓</p>
             <h1>Welcome to Jordan Bortner's fullstack development portfolio</h1>
             <p>░▒▓▒░▒▓▒░▒▓▒░</p>
-            <p>Please be sure to check out the About and Projects pages for more information!</p>
+            <p>Please be sure to check out the About and Projects pages for more information! You can also reach out to me directly by using the contact form below, or by <a href="mailto:jordan.bortner@gmail.com">emailing me</a>.</p>
         </main>
     )
 }
